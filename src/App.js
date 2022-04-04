2300f13c2d51964a3762a283daf21ae5dc53e5ca
BVBFD <lsevina126@gmail.com>
2022-04-04 18:43:23 +0900

    Code pagination by using paginate-react library

--- src/App.js
+++ src/App.js
@@ -1,96 +1,57 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Pagination } from 'swiper';
+import ReactPaginate from 'react-paginate';
 import dummyDatas from './dummyDatas.js';
-import { useEffect, useState } from 'react';
 
-function App() {
-  // const [totalPagesNums, setTotalPagesNums] = useState([]);
-  const [chosenPagesNums, setChosenPagesNums] = useState([1, 2, 3, 4]);
+const App = (props) => {
+  const [pageCount, setpageCount] = useState();
+  const [titleShown, setTitleShown] = useState([0, 1, 2, 3]);
 
-  // useEffect(() => {
-  //   const getPosts = () => {
-  //     let numsArray = [];
-  //     for (let i = 1; i <= Math.ceil(dummyDatas.length / 4); i++) {
-  //       numsArray.push(i);
-  //       setTotalPagesNums(numsArray);
-  //     }
-  //   };
-
-  //   getPosts();
-  // }, [dummyDatas, chosenPagesNums]);
-
-  useEffect(() => {}, [chosenPagesNums]);
-
-  const pagination = {
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
-    },
-  };
-
-  const nextBtn = () => {
-    const nextArray = chosenPagesNums.map((num) => num + 4);
+  useEffect(() => {
     // prettier-ignore
-    if (dummyDatas[(nextArray.at(0) * 4) - 4] === undefined) {
-      return;
-    }
-    setChosenPagesNums(nextArray);
-    const spanNums = document.querySelectorAll('.swiper-pagination-bullet');
-    spanNums.forEach((spanNum) => {
-      // prettier-ignore
-      // console.log(parseInt(spanNum.innerText) + 8);
-      // console.log(parseInt(spanNums[3].innerText));
-      // console.log(totalPagesNums.at(-1));
-      // prettier-ignore
-      return parseInt(spanNum.innerText = parseInt(spanNum.innerText) + 4);
-    });
+    setpageCount(Math.ceil(dummyDatas.length / 4))
+  }, [dummyDatas, pageCount]);
+
+  const handlePageClick = (event) => {
+    // prettier-ignore
+    const newArray = [(event.selected) * 4, ((event.selected) * 4) + 1, ((event.selected) * 4) + 2, ((event.selected) * 4) + 3];
+    setTitleShown(newArray);
   };
 
-  const prevBtn = () => {
-    const prevArray = chosenPagesNums.map((num) => num - 4);
-    if (prevArray.at(0) < 1) {
-      return;
-    }
-    setChosenPagesNums(prevArray);
-    const spanNums = document.querySelectorAll('.swiper-pagination-bullet');
-    spanNums.forEach((spanNum) => {
-      return (spanNum.innerText = parseInt(spanNum.innerText) - 4);
-    });
-  };
+  console.log(titleShown);
 
   return (
     <div className='App'>
-      <Swiper
-        pagination={pagination}
-        modules={[Pagination]}
-        className='mySwiper'
-      >
-        <span onClick={prevBtn} className='navigationButton prevBtn'>
-          앞
-        </span>
-        {chosenPagesNums.map((num) => (
-          <SwiperSlide>
-            <div className='page'>
-              {/* prettier-ignore */}
-              <div>{dummyDatas[(num * 4) - 4] ? dummyDatas[(num * 4) - 4].title : null}</div>
-              {/* prettier-ignore */}
-              <div>{dummyDatas[(num * 4) - 3] ? dummyDatas[(num * 4) - 3].title : null}</div>
-              {/* prettier-ignore */}
-              <div>{dummyDatas[(num * 4) - 2] ? dummyDatas[(num * 4) - 2].title : null}</div>
-              {/* prettier-ignore */}
-              <div>{dummyDatas[(num * 4) - 1] ? dummyDatas[(num * 4) - 1].title : null}</div>
-            </div>
-          </SwiperSlide>
-        ))}
-        <span onClick={nextBtn} className='navigationButton nextBtn'>
-          뒤
-        </span>
-      </Swiper>
+      <div className='titleWrapper'>
+        {titleShown.map((index) => {
+          return dummyDatas[index] !== undefined ? (
+            <div className='title'>{dummyDatas[index].title}</div>
+          ) : (
+            <div className='title'></div>
+          );
+        })}
+      </div>
+      <ReactPaginate
+        previousLabel={'prev'}
+        nextLabel={'next'}
+        breakLabel={''}
+        pageCount={pageCount}
+        marginPagesDisplayed={0}
+        pageRangeDisplayed={4}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination justify-content-center'}
+        pageClassName={'page-item'}
+        pageLinkClassName={'page-link'}
+        previousClassName={'page-item'}
+        previousLinkClassName={'page-link'}
+        nextClassName={'page-item'}
+        nextLinkClassName={'page-link'}
+        breakClassName={'page-item'}
+        breakLinkClassName={'page-link'}
+        activeClassName={'active'}
+      />
     </div>
   );
-}
+};
 
 export default App;
